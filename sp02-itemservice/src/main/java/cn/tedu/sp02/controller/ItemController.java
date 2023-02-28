63ac41f4dc067eb776ad7a6c64ab385f40943831
hucg <1914903701@qq.com>
2023-02-28 09:04:56 +0800

    提交

---
+++ sp02-itemservice/src/main/java/cn/tedu/sp02/controller/ItemController.java
@@ -0,0 +1,35 @@
+package cn.tedu.sp02.controller;
+
+import cn.tedu.sp01.pojo.Item;
+import cn.tedu.sp01.service.ItemService;
+import cn.tedu.web.util.JsonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/{orderId}")
+    public JsonResult<List<Item>> getItems(@PathVariable String orderId) {
+        log.info("server.port="+port+", orderId="+orderId);
+
+        List<Item> items = itemService.getItems(orderId);
+        return JsonResult.ok(items).msg("port="+port);
+    }
+
+    @PostMapping("/decreaseNumber")
+    public JsonResult decreaseNumber(@RequestBody List<Item> items) {
+        itemService.decreaseNumbers(items);
+        return JsonResult.ok();
+    }
+}
---
+++ sp03-userservice/src/main/java/cn/tedu/sp03/service/UserserviceImpl.java
@@ -0,0 +1,36 @@
+package cn.tedu.sp03.service;
+
+import cn.tedu.sp01.pojo.User;
+import cn.tedu.sp01.service.UserService;
+import cn.tedu.web.util.JsonUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class UserserviceImpl implements UserService {
+    @Value("${sp.user-service.users}")
+    private String userJson;
+
+    @Override
+    public User getUser(Integer id) {
+        log.info("users json string : " + userJson);
+        List<User> users = JsonUtil.from(userJson, new TypeReference<List<User>>() {});
+        for (User u : users) {
+            if (u.getId().equals(id)) {
+                return u;
+            }
+        }
+        return new User(id, "name-"+id, "pwd-"+id);
+    }
+
+    @Override
+    public void addScore(Integer id, Integer score) {
+        // 这里增加积分
+        log.info("user "+id+" - 增加积分 "+score);
+    }
+}
